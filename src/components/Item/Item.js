b93ccceeb16d049385e69978e58cd08122b9302c
Anita Cambon <anita_cambon@hotmail.com>
2023-09-13 23:10:15 -0300

    falta ver todo

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,49 +1,31 @@
-import { useEffect, useState } from 'react'
-import { getProducts } from '../../asyncMock'
+
 import { Link } from 'react-router-dom'
 import './Item.css'
 // import { getRate } from '../../utils/getRate'
 
 const Item = ({ id, nombre, categoria, unidades, precio, img }) => {
-    const [loading, setLoading] = useState(false)
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-        setLoading(true)
-        getProducts()
-            .then((products) => setProducts(products))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false))
-    }, [])
-
-    if (loading) return <p className="" >Cargando...</p>
-
-
-
-    return (<section className="tienda container" >
-        <div className="row align-items-center">
-            <h1 className="greetingTienda">
-                Tienda
-            </h1>
-
-            <article key={id} className="CardItem col">
-                <picture>
-                    <img src={`/img/${img}`} alt={nombre} className="imagenTienda" />
-                </picture>
-                <div className="infoTienda">
-                    <h3 className="nombreTienda">{nombre}</h3>
-                    <span className="precioTienda">${precio}</span>
-                    <span className="unidadesTienda">Stock: {unidades}</span>
-                    <span className="categoriaTienda">{categoria}</span>
-                    <button type="button" className="btn btn-warning"><Link to={`/products/${id}`} className="botonTienda">
-                        Ver más...
-                    </Link></button>
-
-                </div>
-            </article>
-
-        </div>
-    </section>
+    return (
+        <section className="tienda container" >
+            <div className="row align-items-center">
+                <h1 className="greetingTienda">
+                    Tienda
+                </h1>
+                <article key={id} className="CardItem col">
+                    <picture>
+                        <img src={`/img/${img}`} alt={nombre} className="imagenTienda" />
+                    </picture>
+                    <div className="infoTienda">
+                        <h3 className="nombreTienda">{nombre}</h3>
+                        <span className="precioTienda">${precio}</span>
+                        <span className="unidadesTienda">Stock: {unidades}</span>
+                        <span className="categoriaTienda">{categoria}</span>
+                        <button type="button" className="btn btn-warning"><Link to={`/products/${id}`} className="botonTienda">
+                            Ver más...
+                        </Link></button>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
