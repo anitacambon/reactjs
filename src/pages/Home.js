ebeb2ae13f49a6df42dc6c5c4a512acd6509a0e4
Anita Cambon <anita_cambon@hotmail.com>
2023-10-26 20:36:01 -0300

    se agregar carousel y estilos de navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,45 @@
+import CARcaserosGi from './img/CARcaserosGi.jpg'
+import CARdevotoGi from './img/CARdevotoGi.jpg'
+import CARstrikingCaseros from './img/CARstrikingCaseros.jpg'
+import CARstrikingDevoto from './img/CARstrikingDevoto.jpg'
+import './home.css'
+
 const Home = () => {
-    return(
-        <h1 className="greetingHome">
-            Centro de Entrenamiento de Artes Marciales y Deportes de Combate
-        </h1>
+    return (
+        <div >
+            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 class="carousel-caption d-none d-md-block">
+                        Academia MAGNA
+                    </h1>
+                    <p>Centro de Entrenamiento de Artes Marciales y Deportes de Combate</p>
+                </div>
+
+
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-bs-interval="4000">
+                        <img src={CARcaserosGi} className="d-block w-100" alt="imagen caseros"></img>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="4000">
+                        <img src={CARdevotoGi} className="d-block w-100" alt="imagen devoto"></img>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="4000">
+                        <img src={CARstrikingCaseros} className="d-block w-100" alt="striking caseros"></img>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="4000">
+                        <img src={CARstrikingDevoto} className="d-block w-100" alt="striking caseros"></img>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
     )
 }
 
