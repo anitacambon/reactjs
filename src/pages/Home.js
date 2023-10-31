725bb7fc79529d0a62270148aa5010d0d207dc3b
Anita Cambon <anita_cambon@hotmail.com>
2023-10-31 19:44:39 -0300

    nav y car

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,30 +17,6 @@ const Home = () => {
                     <p>Centro de Entrenamiento de Artes Marciales y Deportes de Combate</p>
                 </div>
 
-                <div className="carousel-inner mobile">
-                    <div className="carousel-item active" data-bs-interval="4000">
-                        <img src={CarMobJu} className="d-block w-100" alt="imagen ju"></img>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="4000">
-                        <img src={CarMobAguss} className="d-block w-100" alt="imagen agus"></img>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="4000">
-                        <img src={CARstrikingCaseros} className="d-block w-100" alt="striking caseros"></img>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="4000">
-                        <img src={CARstrikingDevoto} className="d-block w-100" alt="striking caseros"></img>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-
-
                 <div className="carousel-inner">
                     <div className="carousel-item active" data-bs-interval="4000">
                         <img src={CARcaserosGi} className="d-block w-100" alt="imagen caseros"></img>
