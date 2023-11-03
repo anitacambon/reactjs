2f23966dcbd8f1e283dd45a8972f2561b85a7532
Anita Cambon <anita_cambon@hotmail.com>
2023-11-02 22:37:07 -0300

    horarios

---
+++ src/components/Firma/Firma.js
@@ -0,0 +1,11 @@
+import './Firma.css'
+
+const Firma = () => {
+    return (
+        <div >
+            <p className="firma">Ana Cambon</p>
+        </div>
+    )
+}
+
+export default Firma
--- src/pages/Horarios.js
+++ src/pages/Horarios.js
@@ -1,3 +1,9 @@
+import horariosDevoto from './img/horariosDevoto.jpg'
+import horariosCaseros from './img/horariosCaseros.jpg'
+import horariosStrikDev from './img/horariosStrikDev.jpg'
+import horariosStriking from './img/horariosStriking.jpg'
+import './css/Horarios.css'
+
 const Horarios = () => {
     return (
         <div>
@@ -5,7 +11,19 @@ const Horarios = () => {
                 Horarios
             </h1>
 
-        </div>
+            <div className='vista-horarios'>
+                <div>
+                    <h2 className='sede'>Sede Villa Devoto</h2>
+                    <img src={horariosDevoto} className="horarios" alt="horarios devoto"></img>
+                    <img src={horariosStrikDev} className="horarios" alt="horarios devoto striking"></img>
+                </div>
+                <div>
+                    <h2 className='sede'> Sede Caseros</h2>
+                    <img src={horariosCaseros} className="horarios" alt="horario caseros"></img>
+                    <img src={horariosStriking} className="horarios" alt="horarios caseros striking"></img>
+                </div>
+            </div>
+        </div >
     )
 }
 
