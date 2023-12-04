3ea4b234d924b2a027ad41c38c2df5444d57f2d0
Anita Cambon <anita_cambon@hotmail.com>
2023-12-03 23:07:30 -0300

    intento de carousel hover mobile

--- src/components/CarouselMobile/CarouselMobile.js
+++ src/components/CarouselMobile/CarouselMobile.js
@@ -9,10 +9,10 @@ import './CarouselMobile.css'
 const CarouselMobile = () => {
     return (
         <div >
-            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" >
                 <div className="carousel-inner">
                     <div className="carousel-item active" data-bs-interval="3000">
-                        <img src={CarMobJu} className="d-block w-100" alt="imagen ju"></img>
+                        <img src={CarMobJu} className="d-block w-100 mx-auto" alt="imagen ju"></img>
                     </div>
                     <div className="carousel-item" data-bs-interval="3000">
                         <img src={CarMobEscuelita} className="d-block w-100" alt="imagen escuelita"></img>
@@ -30,11 +30,11 @@ const CarouselMobile = () => {
                         <img src={CarMobKick} className="d-block w-100" alt="imagen kick"></img>
                     </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplay" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplay" data-bs-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
